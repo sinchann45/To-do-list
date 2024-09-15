f9bca0e529d8e938e306449dc8e4bc2087232d79
sinchann45 <sinchanndekesuki@gmail.com>
2024-09-15 14:29:16 +0530

    Delete learn directory

--- learn/index10.js
+++
@@ -1,9 +0,0 @@
-let box=document.querySelector(".box");
-
-box.addEventListener('mousemove',()=>{
-    box.style.backgroundColor="red";
-})
-
-box.addEventListener('mouseleave',()=>{
-    box.style.backgroundColor="rgb(29, 151, 151)";
-})
